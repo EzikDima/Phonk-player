7abb583c14a26b6e08d9534f071ac2acd199d2f8
EzikDima <146102832+EzikDima@users.noreply.github.com>
2024-03-21 17:53:12 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,23 @@
-document.addEventListener('DOMContentLoaded', function() {
-  let songSelect = document.getElementById('songSelect');
-  let mySong = document.getElementById('mySong');
-  let slider = document.querySelector(".slider");
-
-  songSelect.addEventListener('change', function() {
-    mySong.volume = 100;
-    mySong.src = this.value;
-    mySong.play();
-  });
-  
-  let state = false;
-  let btn = document.querySelector(".btn");
-  let record = document.querySelector(".record");
-  let toneArm = document.querySelector(".tone-arm");
-
-  btn.addEventListener("click", () => {
-    if (state == false) {
-      record.classList.add("on");
-      toneArm.classList.add("play");
-      setTimeout(() => {
-        mySong.play();
-      }, 1000);
-    } else {
-      record.classList.remove("on");
-      toneArm.classList.remove("play");
-      mySong.pause();
-    }
-    state = !state;
-  });
-
-  // Добавьте обработчик событий для слайдера, чтобы изменить громкость аудио
-  slider.addEventListener('input', function() {
-    mySong.volume = this.value / 100; // Значение слайдера должно быть между 0 и 100
-  });
+let state = false;
+let btn = document.querySelector(".btn");
+let record = document.querySelector(".record");
+let toneArm = document.querySelector(".tone-arm");
+let song = document.querySelector(".my-song");
+let slider = document.querySelector(".slider");
+btn.addEventListener("click", () => {
+  if (state == false) {
+    record.classList.add("on");
+    toneArm.classList.add("play");
+    setTimeout(() => {
+      song.play();
+    }, 1000);
+  } else {
+    record.classList.remove("on");
+    toneArm.classList.remove("play");
+    song.pause();
+  }
+  state = !state;
+});
+slider.addEventListener("input", (e) => {
+  song.volume = Number(e.target.value);
 });
